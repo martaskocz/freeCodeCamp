4b9101321854669adb3c45a4fd47efd16b6022bf
Marta <marta.sobstyl@gmail.com>
2017-11-27 16:58:56 +0100

    FCC3

---
+++ intermediateAlgorithmScripting.js
@@ -0,0 +1,20 @@
+// -------->> 1 Sum All Numbers in a Range Complete <<--------
+// -------->> 2 Diff Two Arrays <<--------
+// -------->> 3 Roman Numeral Converter <<--------
+// -------->> 4 Wherefore art thou <<--------
+// -------->> 5 Search and Replace <<--------
+// -------->> 6 Pig Latin <<--------
+// -------->> 7 DNA Pairing <<--------
+// -------->> 8 Missing letters <<--------
+// -------->> 9 Boo who <<--------
+// -------->> 10 Convert HTML Entities <<--------
+// -------->> 11 Spinal Tap Case <<--------
+// -------->> 12 Sum All Odd Fibonacci Numbers <<--------
+// -------->> 13 Sum All Primes <<--------
+// -------->> 14 Smallest Common Multiple <<--------
+// -------->> 15 Finders Keepers <<--------
+// -------->> 16 Drop it <<--------
+// -------->> 17 Steamroller <<--------
+// -------->> 18 Binary Agents <<--------
+// -------->> 19 Everything Be True <<--------
+// -------->> 20 Arguments Optional <<--------

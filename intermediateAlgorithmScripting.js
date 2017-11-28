5dc53103c0e3512487b23da703d429989a20ab61
Marta <marta.sobstyl@gmail.com>
2017-11-28 12:53:07 +0100

    FCC4

--- intermediateAlgorithmScripting.js
+++ intermediateAlgorithmScripting.js
@@ -1,6 +1,35 @@
 // -------->> 1 Sum All Numbers in a Range Complete <<--------
+//We'll pass you an array of two numbers. Return the sum of those two numbers and all numbers between them.
+
+function sumAll(arr) {
+  var sum = 0;
+  for (var i = Math.min(...arr); i<=Math.max(...arr); i++){
+    sum +=i;
+  }
+  return sum;
+}
+sumAll([1, 4]);
+
 // -------->> 2 Diff Two Arrays <<--------
+//Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both. 
+//In other words, return the symmetric difference of the two arrays.
+
+function diffArray(arr1, arr2) {
+  return arr1
+    .concat(arr2)
+    .filter(item => !arr1.includes(item) || !arr2.includes(item));
+}
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+
 // -------->> 3 Roman Numeral Converter <<--------
+
+function convertToRoman(num) {
+ var decimalValue = [1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000];
+ var romanValue = ['I', 'IV', 'V', 'IX', 'X', 'XL', 'L', 'XC', 'C', 'CD', 'D'];
+}
+
+convertToRoman(36);
+
 // -------->> 4 Wherefore art thou <<--------
 // -------->> 5 Search and Replace <<--------
 // -------->> 6 Pig Latin <<--------
